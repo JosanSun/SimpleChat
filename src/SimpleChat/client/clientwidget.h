b64b8c0bba69668b6ab87620c96c675391641100
Josan <josancpp@gmail.com>
2018-04-03 01:19:17 +0800

    add the file transfer function

--- src/SimpleChat/client/clientwidget.h
+++ src/SimpleChat/client/clientwidget.h
@@ -3,6 +3,8 @@
 
 #include <QWidget>
 #include <QTcpSocket>   //通信套接字
+#include <QUdpSocket>
+#include <QFile>
 
 namespace Ui {
 class ClientWidget;
@@ -20,10 +22,25 @@ private slots:
     void on_buttonConnect_clicked();
     void on_buttonSend_clicked();
     void on_buttonClose_clicked();
+    void dealMsg();
 
 private:
     Ui::ClientWidget *ui  = nullptr;
     QTcpSocket* tcpSocket = nullptr;
+    QUdpSocket* udpSocket = nullptr;
+
+    QString localAddress;
+    QString peerAddress;
+    quint16 localTcpPort;
+    quint16 peerTcpPort;
+    quint16 localUdpPort;
+    quint16 peerUdpPort;
+
+    QFile file;
+    QString fileName;
+    qint64 fileSize;
+    qint64 recvSize;
+    bool isStart = true;
 };
 
 #endif // CLIENTWIDGET_H
